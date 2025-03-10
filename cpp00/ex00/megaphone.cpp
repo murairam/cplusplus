2f39fb18003892068ddece3367c610704368a7ff
murairam <marimiilpalu@gmail.com>
2025-03-10 15:35:04 +0100

    exo1 repairs

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:33:14 by mmiilpal          #+#    #+#             */
-/*   Updated: 2025/03/10 13:50:37 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2025/03/10 14:06:30 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,12 @@ int main(int argc, char **argv)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
     }
-	for (int i = 1; i < argc; i++)
-	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			std::cout << (char)std::toupper(argv[i][j]);
-		}
-	}
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];  // Convert char* to std::string
+        for (size_t j = 0; j < arg.length(); j++)
+            std::cout << (char)std::toupper(arg[j]);
+    }
     std::cout << std::endl;
 	return 0;
 }
