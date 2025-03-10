6dd2c08313f18074bb1dfdac6a4fc0808e94ab88
murairam <marimiilpalu@gmail.com>
2025-03-10 13:52:08 +0100

    exo 00 done i think

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,27 @@
 /*   By: mmiilpal <mmiilpal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:33:14 by mmiilpal          #+#    #+#             */
-/*   Updated: 2025/03/10 13:33:15 by mmiilpal         ###   ########.fr       */
+/*   Updated: 2025/03/10 13:50:37 by mmiilpal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+    }
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+		}
+	}
+    std::cout << std::endl;
+	return 0;
+}
