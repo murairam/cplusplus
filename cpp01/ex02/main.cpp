c1084c6c22d73080f414c31418a391ec8a418037
murairam <marimiilpalu@gmail.com>
2025-04-21 17:54:39 +0200

    ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string brain = "HI THIS IS BRAIN";
+    std::string* stringPTR = &brain;   // pointer to brain
+    std::string& stringREF = brain;    // reference to brain
+
+    std::cout << "Memory address of brain:      " << &brain << std::endl;
+    std::cout << "Memory address held by PTR:   " << stringPTR << std::endl;
+    std::cout << "Memory address held by REF:   " << &stringREF << std::endl;
+
+    std::cout << "Value of brain:               " << brain << std::endl;
+    std::cout << "Value pointed to by PTR:      " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by REF:      " << stringREF << std::endl;
+    return 0;
+}
