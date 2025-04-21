96927b7a143d357c2b77a3cf337422422091b01d
murairam <marimiilpalu@gmail.com>
2025-04-21 17:06:44 +0200

    cpp01 ex00

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,21 @@
+
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+
+class Zombie{
+    private:
+        std::string name;
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+
+        void announce(void);
+};
+
+#endif
---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+int main() {
+
+    std::cout << "--- Heap Zombie Test ---" << std::endl;
+    Zombie* zombie = newZombie("Hiro the Gutless");
+    zombie->announce();
+    delete zombie;
+
+    std::cout << "--- Stack Zombie Test ---" << std::endl;
+    randomChump("Club Foot");
+    randomChump("BOB");
+    return 0;
+}
