907ef3df0aff38c798ce1f101d20b9a4ecc28253
murairam <marimiilpalu@gmail.com>
2025-08-08 19:57:03 +0200

    okei

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -1,13 +1,13 @@
 #include "Zombie.hpp"
 
 Zombie::Zombie(std::string name){
-    this->name = name;
+    this->_name = name;
 }
 
 Zombie::~Zombie() {
-    std::cout << name << " is dead!" << std::endl;
+    std::cout << _name << " is dead!" << std::endl;
 }
 
 void Zombie::announce(){
-    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
